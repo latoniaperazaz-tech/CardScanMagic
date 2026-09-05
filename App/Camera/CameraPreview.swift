03f105804ae007c7dc04879021f5909d0d7c7570
latoniaperazaz-tech <latoniaperazaz@gmail.com>
2026-09-05 15:29:09 +0800

    Add CardScanMagic source

---
+++ App/Camera/CameraPreview.swift
@@ -0,0 +1,32 @@
+import AVFoundation
+import SwiftUI
+import UIKit
+
+struct CameraPreview: UIViewRepresentable {
+    let session: AVCaptureSession
+
+    func makeUIView(context: Context) -> PreviewView {
+        let view = PreviewView()
+        view.previewLayer.session = session
+        view.previewLayer.videoGravity = .resizeAspectFill
+        if let connection = view.previewLayer.connection, connection.isVideoOrientationSupported {
+            connection.videoOrientation = .portrait
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: PreviewView, context: Context) {
+        uiView.previewLayer.session = session
+    }
+}
+
+final class PreviewView: UIView {
+    override class var layerClass: AnyClass { AVCaptureVideoPreviewLayer.self }
+
+    var previewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("PreviewView must use AVCaptureVideoPreviewLayer")
+        }
+        return layer
+    }
+}
